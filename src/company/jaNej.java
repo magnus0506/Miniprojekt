402ed7fb9a202f11c9df97a529cf2c0fc828da5e
magnus0506 <47506157+magnus0506@users.noreply.github.com>
2020-04-03 20:48:42 +0200

    Initial Commit

--- src/company/jaNej.java
+++ src/company/jaNej.java
@@ -1,32 +1,26 @@
 package company;
-
 import java.util.Scanner;
 
 public class jaNej {
+    private boolean returnValue;
+    private  String jaNej;
+    private char jn;
+    Scanner input = new Scanner(System.in);
 
-public boolean jaNej(){
-    boolean returnValue;
-    String jaNej;
-    char jn;
-Scanner input = new Scanner(System.in);
-do{
-jaNej = input.next().toLowerCase();
-jn = jaNej.charAt(0);
-} while (jn != 'j' && jn != 'n');
+    public boolean jaNej(){
+        do{
+            jaNej = input.next().toLowerCase();
+            jn = jaNej.charAt(0);
+        } while (jn != 'j' && jn != 'n');
 
-if(jn=='j'){
-    returnValue = true;
-} else if(jn == 'n'){
-returnValue = false;
-}else{
-    System.out.println("Det indtastede er forkert, prøv igen");
-returnValue = false;
-}
-return returnValue;
-
-
-
-}
-
-
+        if(jn=='j'){
+            returnValue = true;
+        } else if(jn == 'n'){
+        returnValue = false;
+        }else{
+            System.out.println("Det indtastede er forkert, prøv igen");
+            returnValue = false;
+        }
+        return returnValue;
+    }
 }
