851a2c44a0b3bb2b3c80a85545d867c10ee0146e
magnus0506 <47506157+magnus0506@users.noreply.github.com>
2020-04-03 20:59:56 +0200

    Initial Commit

--- src/company/Main.java
+++ src/company/Main.java
@@ -1,28 +1,13 @@
 package company;
-
 import java.io.IOException;
 
 public class Main {
-
-    //to-do, login, menu uddybninger
-    //hvad sker der hvis man skriver forkert?
-    //evt nedarvning
-
     public static void main(String[] args) throws IOException {
-	// write your code here
-
-        barnCrud barnecrud = new barnCrud();
-        Menu menu = new Menu();
         tekstfil tekstFil = new tekstfil();
         Login login = new Login();
-        //tekstFil.readFile();
         tekstFil.readFile();
         tekstFil.readFileParents();
         login.systemLogin();
         tekstFil.writeFile();
-
-
-
-
     }
 }
