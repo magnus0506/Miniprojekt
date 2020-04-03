39235b9490bc84a9019049ad27dfcc1d33f9bd60
magnus0506 <47506157+magnus0506@users.noreply.github.com>
2020-04-03 21:03:18 +0200

    Initial Commit

--- src/company/Parents.java
+++ src/company/Parents.java
@@ -1,26 +1,26 @@
 package company;
 
 public class Parents extends Barn {
-    int tlfnr;
-    public Parents(String firstName, String lastName, String birthday, int id, int tlfnr) {
-        super(firstName, lastName, birthday, id);
-        this.tlfnr = tlfnr;
+    private int tlfNr;
 
+    public Parents(String firstName, String lastName, String birthday, int id, int tlfNr) {
+        super(firstName, lastName, birthday, id);
+        this.tlfNr = tlfNr;
             }
 
     @Override
     public String toString() {
         return "ParentsContactList{" +
-                "tlfnr=" + tlfnr +
+                "tlfnr=" + tlfNr +
                 '}';
     }
 
     public int getTlfnr() {
-        return tlfnr;
+        return tlfNr;
     }
 
     public void setTlfnr(int tlfnr) {
-        this.tlfnr = tlfnr;
+        this.tlfNr = tlfnr;
     }
 }
 
